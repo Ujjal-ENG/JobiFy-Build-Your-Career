a9eb7ef84f2bd4daceb852b22b56a834ed0deca6
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-24 15:28:14 +0600

    done the connection of mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,26 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-extraneous-dependencies */
 // imports
 import dotenv from 'dotenv';
 import express from 'express';
+import connectDB from './config/connectDB.js';
 
 // configure dotenv
-dotenv.config()
+dotenv.config();
+
+// db connection
+connectDB();
 // rest object
 const app = express();
 // default route
 app.get('/', (req, res) => {
     res.send('Hello There i am from default routes!!!');
 });
-
-
 // server listen port
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 // listen
 app.listen(PORT, () => {
-  console.log("Server is listen at 8080 PORT")
-})
+    console.log('Server is listen at 8080 PORT');
+});
