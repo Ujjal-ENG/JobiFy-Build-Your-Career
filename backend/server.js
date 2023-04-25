04ebd91e04c3fc6cc14d1a47c993f6608fb537a8
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-25 11:30:57 +0600

    apply santize for protecting mongodb database againt the sql injection

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import 'express-async-errors';
+import mongoSanitize from 'express-mongo-sanitize';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import xss from 'xss-clean';
@@ -26,6 +27,7 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(xss());
+app.use(mongoSanitize());
 app.use(cors());
 app.use(morgan('dev'));
 
