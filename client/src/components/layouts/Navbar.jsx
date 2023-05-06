b77f81897c3b1cda6317357e834ac7ec73bc7d5d
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-07 04:22:24 +0600

    now its worked perfectly

--- client/src/components/layouts/Navbar.jsx
+++ client/src/components/layouts/Navbar.jsx
@@ -4,16 +4,16 @@
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable react/jsx-closing-bracket-location */
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
     return (
         <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <Link to="/" className="flex items-center">
+                <NavLink to="/" className="flex items-center">
                     <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Job Tracking App</span>
-                </Link>
+                </NavLink>
                 <div className="flex md:order-2">
                     <button
                         type="button"
@@ -39,37 +39,39 @@ function Navbar() {
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
                     <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
-                            <Link to="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">
+                            <NavLink
+                                to="/"
+                                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                                 Home
-                            </Link>
+                            </NavLink>
                         </li>
                         <li>
-                            <Link
+                            <NavLink
                                 to="/about"
                                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                                 About
-                            </Link>
+                            </NavLink>
                         </li>
                         <li>
-                            <Link
+                            <NavLink
                                 to="/service"
                                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                                 Services
-                            </Link>
+                            </NavLink>
                         </li>
                         <li>
-                            <Link
+                            <NavLink
                                 to="/contact"
                                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                                 Contact
-                            </Link>
+                            </NavLink>
                         </li>
                         <li>
-                            <Link
+                            <NavLink
                                 to="/login"
                                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                                 Login
-                            </Link>
+                            </NavLink>
                         </li>
                     </ul>
                 </div>
--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,8 +1,52 @@
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable react/jsx-indent */
 /* eslint-disable react/jsx-indent-props */
 import React from 'react';
+import { Link } from 'react-router-dom';
+import BackgroundVideo from '../../assets/pexels-fauxels-3249673-1920x1080-50fps.mp4';
 
 function Home() {
-    return <>fdkjf</>;
+    return (
+        <>
+            <video autoPlay muted loop className="fixed top-0 left-0 w-full h-full object-cover z-0">
+                <source src={BackgroundVideo} type="video/mp4" />
+            </video>
+            <div className="absolute top-0 left-0 w-full h-full bg-gray-900 bg-opacity-60" />
+
+            <div className="relative z-10  pt-10">
+                <div className="mt-20 ml-10 h-[70vh] flex justify-start items-center">
+                    <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="flex items-center mb-6 text-2xl font-semibold justify-center w-full text-gray-900 dark:text-white">
+                            <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+                            Job Tracking App
+                        </div>
+                        <div className="flex flex-col space-y-2 pb-4 items-baseline text-gray-900 dark:text-white w-full">
+                            <h3 className="text-2xl font-bold tracking-wider py-3">Bangladesh No #1 Job Seeking Plat form</h3>
+
+                            <p className="text-gray-500">Search and Manage your Jobs with ease, free and open source job tracking application by This app</p>
+                        </div>
+
+                        <div className="flex justify-between items-center">
+                            <p className=" font-bold text-amber-400">Not a User? Please Register First..</p>
+                            <Link
+                                to="/register"
+                                type="button"
+                                className="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
+                                <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                                <span className="sr-only">Icon description</span>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Home;
