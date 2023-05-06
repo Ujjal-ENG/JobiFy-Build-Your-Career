d8ad3a5f7b9bbdf8b808b9bd3e48c00345f89cf7
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-07 03:41:48 +0600

    register page done

--- client/src/components/pages/Login&Register/Register.jsx
+++ client/src/components/pages/Login&Register/Register.jsx
@@ -1,12 +1,31 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/jsx-indent */
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable react/jsx-closing-bracket-location */
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Register() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        location: ''
+    });
+
+    const handleChange = (e) => {
+        setFormData((ps) => ({
+            ...ps,
+            [e.target.id]: e.target.value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
         <div>
             <section className="bg-gray-50 dark:bg-gray-900">
@@ -18,7 +37,7 @@ function Register() {
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Create and account</h1>
-                            <form className="space-y-4 md:space-y-6" action="#">
+                            <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6" action="#">
                                 <div>
                                     <label htmlFor="Name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                         Your Name
@@ -30,6 +49,8 @@ function Register() {
                                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         placeholder="Jom Dow"
                                         required
+                                        value={formData.name}
+                                        onChange={handleChange}
                                     />
                                 </div>
                                 <div>
@@ -43,6 +64,8 @@ function Register() {
                                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         placeholder="name@company.com"
                                         required
+                                        value={formData.email}
+                                        onChange={handleChange}
                                     />
                                 </div>
                                 <div>
@@ -56,6 +79,8 @@ function Register() {
                                         placeholder="••••••••"
                                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         required
+                                        value={formData.password}
+                                        onChange={handleChange}
                                     />
                                 </div>
                                 <div>
@@ -69,6 +94,8 @@ function Register() {
                                         placeholder="Dhaka"
                                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         required
+                                        value={formData.location}
+                                        onChange={handleChange}
                                     />
                                 </div>
                                 <div className="flex items-start">
@@ -82,11 +109,8 @@ function Register() {
                                         />
                                     </div>
                                     <div className="ml-3 text-sm">
-                                        <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">
-                                            I accept the
-                                            <Link className="font-medium text-primary-600 hover:underline dark:text-primary-500" to="/">
-                                                Terms and Conditions
-                                            </Link>
+                                        <label htmlFor="terms" className="font-bold  text-blue-500 dark:text-gray-300">
+                                            I accept the Terms and Conditions
                                         </label>
                                     </div>
                                 </div>
