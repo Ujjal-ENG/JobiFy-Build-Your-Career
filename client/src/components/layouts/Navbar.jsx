9bca9ca678de9d5628ddae8a2716e5e8030ce8b2
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-08 00:12:36 +0600

    done the simple modification

--- client/src/components/layouts/Navbar.jsx
+++ client/src/components/layouts/Navbar.jsx
@@ -14,6 +14,16 @@ function Navbar() {
     return (
         <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <div className="text-center">
+                    <button
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        type="button"
+                        data-drawer-target="drawer-navigation"
+                        data-drawer-show="drawer-navigation"
+                        aria-controls="drawer-navigation">
+                        Show Menu
+                    </button>
+                </div>
                 <NavLink to="/" className="flex items-center">
                     <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Job Tracking App</span>
--- client/src/components/pages/Menu/Menu.jsx
+++ client/src/components/pages/Menu/Menu.jsx
@@ -9,17 +9,6 @@ import { Link } from 'react-router-dom';
 function Menu() {
     return (
         <div>
-            <div className="text-center">
-                <button
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                    type="button"
-                    data-drawer-target="drawer-navigation"
-                    data-drawer-show="drawer-navigation"
-                    aria-controls="drawer-navigation">
-                    Show Menu
-                </button>
-            </div>
-
             <div
                 id="drawer-navigation"
                 className="fixed top-20 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-80 dark:bg-gray-800"
