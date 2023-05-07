e1ecf1911d552ad1e413be7036c29295396d78c4
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-07 16:19:51 +0600

    axios added for calling hte server

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 import { configureStore } from '@reduxjs/toolkit';
 import alertReducer from './features/alertSlice';
+import authReducer from './features/auth/authSlice';
 
 export const store = configureStore({
   reducer: {
     alert: alertReducer,
+    auth: authReducer,
   },
 });
